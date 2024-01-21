df9a3330c1f4d95fa2ca08858d486a17d8eaac18
gilzilberfeld <gil.zilberfeld@gmail.com>
2024-01-21 14:43:49 +0200

    d01

---
+++ src/demos/d01.coverage/creditcard.ts
@@ -0,0 +1,18 @@
+export default class CreditCard {
+
+  expirationDate : Date;
+
+  constructor(  expirationDate : Date) { 
+    this.expirationDate =  expirationDate;
+  }
+
+  isExpired() : boolean{
+		const today = new Date()
+
+		if (today < this.expirationDate )
+			return true;
+		else
+			return false;
+	}
+
+}
