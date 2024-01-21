110df7f70ad813053cf6cc44d1b26cbc391a072c
gilzilberfeld <gil.zilberfeld@gmail.com>
2024-01-21 16:07:50 +0200

    parameterized

---
+++ src/demos/d02.factorial/factorial.parameterized.test.ts
@@ -0,0 +1,27 @@
+import exp from "constants";
+import Factorial from "./factorial";
+
+describe("Factorial data-driven] ", () => {
+  let fact: Factorial;
+
+  beforeEach(() => {
+    fact = new Factorial();
+  });
+
+  test.each([
+    [1, 1],
+    [3, 6],
+    [6, 720],
+  ])("should calculate for %p with expected %p", (input, expected) => {
+    expect(fact.calculate(input)).toBe(expected);
+  });
+
+  test.each`
+  input   | expected
+  ${1}    | ${1}
+  ${3}    | ${6}
+  ${6}    | ${720}
+  `("should calculate for $input with expected $expected ", ({input, expected}) => {
+    expect(fact.calculate(input)).toBe(expected);
+  });
+});
