2d7400b9cf037227f2c51ebc5d3789a8815abae4
gilzilberfeld <gil.zilberfeld@gmail.com>
2024-01-23 14:33:48 +0200

    jest.fn mock

--- src/demos/d03.mocking/jest/driver.jest.partial.test.ts
+++ src/demos/d03.mocking/jest/driver.jest.partial.test.ts
@@ -4,22 +4,26 @@ import { Driver } from "../driver";
 
 describe('Driver with Jest partial mocking', () => {
   let driver: Driver;
-  let mockCar : Car;
+  let realCar : Car;
 
   beforeEach(() => {
-    mockCar = new Car();
-    driver = new Driver(mockCar);
+    realCar = new Car();
+    driver = new Driver(realCar);
   });
 
-  afterEach(() => {
-
-  })
-
   it('real car, mocked method for a while', () => {
-    const spy = jest.spyOn(mockCar, "isRunning").mockImplementation(() => true);
+    const spy = jest.spyOn(realCar, "isRunning").mockImplementation(() => true);
     expect(driver.canDrive()).toBeFalsy();
     spy.mockRestore();
     expect(()=> driver.canDrive()).toThrow('Not yet');
   });
-
+  
+  it('real car, mocked method', ()=>{
+    realCar.isRunning = jest.fn(()=> true);
+    expect(driver.canDrive()).toBeFalsy();
+  })
+  
+  it('real car, method not mocked this time', ()=>{
+      expect(()=> driver.canDrive()).toThrow('Not yet');
+    })
 });
