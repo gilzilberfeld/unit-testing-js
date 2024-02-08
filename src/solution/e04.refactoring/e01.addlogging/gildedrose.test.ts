a0ccdf98bfcd25178aff562f1726cb5bda7baf18
gilzilberfeld <gil.zilberfeld@gmail.com>
2024-02-08 13:02:26 +0100

    gildedrose 1

---
+++ src/solution/e04.refactoring/e01.addlogging/gildedrose.test.ts
@@ -0,0 +1,38 @@
+import { GildedRose } from "./gildedrose";
+import { Item } from "./item";
+
+describe("Gilded Rose", () => {
+  const items = [
+    new Item("+5 Dexterity Vest", 10, 20), //
+    new Item("Aged Brie", 2, 0), //
+    new Item("Elixir of the Mongoose", 5, 7), //
+    new Item("Sulfuras, Hand of Ragnaros", 0, 80), //
+    new Item("Sulfuras, Hand of Ragnaros", -1, 80),
+    new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
+    new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49),
+    new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49),
+  ];
+
+  function printItems(): string {
+    let itemLog: string = "name, sellIn, quality\n";
+    for (let item of items) {
+      itemLog += item.toString();
+    }
+    itemLog += "\n";
+
+    return itemLog;
+  }
+
+  it("30 days of shopping", () => {
+    const gildedRose = new GildedRose(items);
+    let log = "";
+
+    for (let day = 0; day < 30; day++) {
+      log += "Day " + day + "\n";
+      log += printItems();
+      gildedRose.updateQuality();
+      log += "\n";
+    }
+    expect(log).toMatchSnapshot();
+  });
+});
