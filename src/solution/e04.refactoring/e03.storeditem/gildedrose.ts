a449995110cf0234a80a67129a6a2e99e38bc2b5
gilzilberfeld <gil.zilberfeld@gmail.com>
2024-02-10 13:46:35 +0200

    add stored items

---
+++ src/solution/e04.refactoring/e03.storeditem/gildedrose.ts
@@ -0,0 +1,55 @@
+import { AgedBrie } from "./agedbrie";
+import { Backstage } from "./backstage";
+import { ElixirOrVest } from "./elixirorvest";
+import { Item } from "./item";
+import { StoredItem } from "./storeditem";
+import { Sulfuras } from "./sulfuras";
+
+export class GildedRose {
+  items: Array<Item>;
+
+  constructor(items = [] as Array<Item>) {
+    this.items = items;
+  }
+
+  updateQuality() {
+    for (let i = 0; i < this.items.length; i++) {
+      this.updateItem(this.items[i]);
+    }
+    return this.items;
+  }
+
+  private updateItem(item: Item) {
+    let storedItem: StoredItem;
+    switch (item.name) {
+      case "Elixir of the Mongoose":
+      case "+5 Dexterity Vest":
+        storedItem = new ElixirOrVest(item);
+        storedItem.update();
+        break;
+      case "Aged Brie":
+        storedItem = new AgedBrie(item);
+        storedItem.update();
+        break;
+      case "Backstage passes to a TAFKAL80ETC concert":
+        storedItem = new Backstage(item);
+        storedItem.update();
+        break;
+      case "Sulfuras, Hand of Ragnaros":
+        storedItem = new Sulfuras(item);
+        storedItem.update();
+    }
+  }
+
+  private decreaseSellIn(item: Item) {
+    item.sellIn = item.sellIn - 1;
+  }
+
+  private increaseQuality(item: Item) {
+    item.quality = item.quality + 1;
+  }
+
+  private decreaseQuality(item: Item) {
+    item.quality = item.quality - 1;
+  }
+}
